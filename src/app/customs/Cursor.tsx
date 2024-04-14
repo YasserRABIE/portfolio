03d2cd2b7372001d2adec4d93e92805f2fa3c63a
yasser110306 <yasserrabie110306@gmail.com>
2024-04-14 07:07:21 +0200

    fix cursor

--- src/app/customs/Cursor.tsx
+++ src/app/customs/Cursor.tsx
@@ -1,95 +1,63 @@
 "use client";
-import { AnimatePresence, motion, useMotionValue } from "framer-motion";
-import { ArrowUpRight } from "lucide-react";
-import React, { useEffect } from "react";
+import { motion } from "framer-motion";
+import React, { useEffect, useRef } from "react";
+import { gsap } from "gsap";
 
 export default function Cursor({ isHovering, hoveringProject }: { isHovering: boolean; hoveringProject: boolean }) {
+    const cursor = useRef(null);
+    const projectsCursor = useRef(null);
     const cursorVariants = {
         initial: {
             scale: 1,
         },
         animate: {
-            scale: hoveringProject ? 5 : isHovering ? 13 : 1,
+            scale: isHovering ? 13 : 1,
             transition: {
                 type: "tween",
                 duration: 0.6,
                 ease: "backOut",
             },
         },
-        exit: {
-            scale: 1,
-            transition: {
-                type: "tween",
-                duration: 0.6,
-                ease: "backInOut",
-            },
-        },
     };
 
     const size = 20;
 
-    const mouse = {
-        x: useMotionValue(0),
-        y: useMotionValue(0),
-    };
-
-    const smoothOptions = { damping: 30, stiffness: 300, mass: 0.4 };
-
-    const smoothMouse = {
-        x: mouse.x,
-        y: mouse.y,
-    };
-
-    const manageMouseMove = (e: MouseEvent) => {
-        const { clientX, clientY } = e;
-        mouse.x.set(clientX - size / 2 + size);
-        mouse.y.set(clientY - size / 2 + size);
-    };
-
     useEffect(() => {
-        window.addEventListener("mousemove", manageMouseMove);
-        return () => window.removeEventListener("mousemove", manageMouseMove);
+        const xMoveCursor = gsap.quickTo(cursor.current, "left", { duration: 0.5, ease: "power3" });
+        const yMoveCursor = gsap.quickTo(cursor.current, "top", { duration: 0.5, ease: "power3" });
+        const xMoveProjectsCursor = gsap.quickTo(projectsCursor.current, "left", { duration: 0.45, ease: "power3" });
+        const yMoveProjectsCursor = gsap.quickTo(projectsCursor.current, "top", { duration: 0.45, ease: "power3" });
+
+        window.addEventListener("mousemove", (e) => {
+            const { clientX, clientY } = e;
+            xMoveCursor(clientX - size / 2);
+            yMoveCursor(clientY - size / 2);
+            xMoveProjectsCursor(clientX - size / 2);
+            yMoveProjectsCursor(clientY - size / 2);
+        });
+        return () =>
+            window.removeEventListener("mousemove", (e) => {
+                const { clientX, clientY } = e;
+                xMoveCursor(clientX);
+                yMoveCursor(clientY);
+                xMoveProjectsCursor(clientX);
+                yMoveProjectsCursor(clientY);
+            });
     });
     return (
-        <>
-            {!hoveringProject && (
-                <motion.div
-                    variants={cursorVariants}
-                    initial="initial"
-                    animate="animate"
-                    style={{
-                        width: size,
-                        height: size,
-                        x: smoothMouse.x,
-                        y: smoothMouse.y,
-                        zIndex: 999999,
-                        backgroundColor: "white",
-                        mixBlendMode: "exclusion",
-                    }}
-                    className="fixed -top-[20px] -left-[20px] rounded-full  pointer-events-none "
-                ></motion.div>
-            )}
-            <AnimatePresence>
-                {hoveringProject && (
-                    <motion.div
-                        variants={cursorVariants}
-                        initial="initial"
-                        animate="animate"
-                        style={{
-                            width: size,
-                            height: size,
-                            x: smoothMouse.x,
-                            y: smoothMouse.y,
-                            zIndex: 999999,
-                            backgroundColor: "#E0E0DF",
-                        }}
-                        exit="exit"
-                        className=" flex items-center justify-center  fixed -top-[20px] -left-[20px] rounded-full  pointer-events-none "
-                    >
-                        <ArrowUpRight size={10} />
-                    </motion.div>
-                )}
-            </AnimatePresence>
-        </>
+        <motion.div
+            ref={cursor}
+            variants={cursorVariants}
+            initial="initial"
+            animate="animate"
+            style={{
+                width: size,
+                height: size,
+                zIndex: 999999,
+                backgroundColor: "white",
+                mixBlendMode: "exclusion",
+            }}
+            className="fixed rounded-full -top-[20px] -left-[20px] pointer-events-none "
+        ></motion.div>
     );
 }
