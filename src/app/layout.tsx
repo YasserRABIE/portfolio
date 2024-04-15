16d72a4cc24c930fb7cbf88528d01525883778ed
yasser110306 <yasserrabie110306@gmail.com>
2024-04-15 17:49:11 +0200

    Update title and description in metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,11 +2,10 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "../app/styles/index.css";
 
-
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "Yasser Web-Developer | BETA",
+    title: "YasserRABIE | Developer && Sudent",
     description: "Yasser Rabie A front-end developer",
 };
 
@@ -15,7 +14,6 @@ export default function RootLayout({
 }: Readonly<{
     children: React.ReactNode;
 }>) {
-    
     return (
         <html lang="en">
             <body className={inter.className}>{children}</body>
