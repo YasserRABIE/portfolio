3be7d1fbcdd616933813ccf1a518c7ce47873480
yasser110306 <yasserrabie110306@gmail.com>
2024-05-06 15:49:33 +0300

    changing projects titles

--- src/app/data/projects.ts
+++ src/app/data/projects.ts
@@ -1,17 +1,17 @@
 export const projects = [
-    {
-        titles: ["EFFADAH-UI", "REACT", "TAILWIND CSS"],
-        link: "https://effadah.netlify.app/",
-        imageSrc: "/assets/images/effadah.jpeg",
-    },
-    {
-        titles: ["LUCIDE", "REACT"],
-        link: "https://effadah.netlify.app/",
-        imageSrc: "/assets/images/luicde.png",
-    },
-    {
-        titles: ["AWWARDS", "NEXT"],
-        link: "https://effadah.netlify.app/",
-        imageSrc: "/assets/images/awwards.png",
-    },
+   {
+      titles: ["EFFADAH-UI"],
+      link: "https://effadah.netlify.app/",
+      imageSrc: "/assets/images/effadah.jpeg",
+   },
+   {
+      titles: ["LUCIDE"],
+      link: "https://effadah.netlify.app/",
+      imageSrc: "/assets/images/luicde.png",
+   },
+   {
+      titles: ["AWWARDS"],
+      link: "https://effadah.netlify.app/",
+      imageSrc: "/assets/images/awwards.png",
+   },
 ];
