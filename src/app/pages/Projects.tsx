7f32d651c196216bbcc2135565285b773379565f
yasser110306 <yasserrabie110306@gmail.com>
2024-05-06 11:44:52 +0300

    Refactor Projects component

--- src/app/pages/Projects.tsx
+++ src/app/pages/Projects.tsx
@@ -5,39 +5,39 @@ import { projects } from "../data/projects";
 import { useScroll } from "framer-motion";
 
 function Projects({ setHoveringProject }: { setHoveringProject: Function }) {
-    const container = useRef(null);
-    const { scrollYProgress } = useScroll({
-        target: container,
-        offset: ["start start", "end end"],
-    });
-    return (
-        <div id="Projects" className="flex flex-col items-center selection:bg-dark-2">
-            <AnimatedSectionTitle Title="projects" />
-            <div
-                ref={container}
-                onMouseEnter={() => {
-                    setHoveringProject(true);
-                }}
-                onMouseLeave={() => {
-                    setHoveringProject(false);
-                }}
-                className="main h-[300vh]"
-            >
-                {projects.map((project, index) => {
-                    const targetScale = 1 - (projects.length - index) * 0.05;
-                    return (
-                        <Card
-                            key={index}
-                            {...project}
-                            progress={scrollYProgress}
-                            range={[index * 0.25, 1]}
-                            targetScale={targetScale}
-                        />
-                    );
-                })}
-            </div>
-        </div>
-    );
+   const container = useRef(null);
+   const { scrollYProgress } = useScroll({
+      target: container,
+      offset: ["start start", "end end"],
+   });
+   return (
+      <div id="Projects" className="flex flex-col items-center selection:bg-dark-2">
+         <AnimatedSectionTitle Title="projects" />
+         <div
+            ref={container}
+            onMouseEnter={() => {
+               setHoveringProject(true);
+            }}
+            onMouseLeave={() => {
+               setHoveringProject(false);
+            }}
+            className="main h-[300vh]"
+         >
+            {projects.map((project, index) => {
+               const targetScale = 1 - (projects.length - index) * 0.05;
+               return (
+                  <Card
+                     key={index}
+                     {...project}
+                     progress={scrollYProgress}
+                     range={[index * 0.25, 1]}
+                     targetScale={targetScale}
+                  />
+               );
+            })}
+         </div>
+      </div>
+   );
 }
 
 export default Projects;
