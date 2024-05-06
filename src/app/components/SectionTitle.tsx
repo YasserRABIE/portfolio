dbfb41f3bd26f57122918093514cffe8365f14f8
yasser110306 <yasserrabie110306@gmail.com>
2024-05-06 11:44:33 +0300

    Refactor section title animation

--- src/app/components/SectionTitle.tsx
+++ src/app/components/SectionTitle.tsx
@@ -2,32 +2,32 @@ import { motion } from "framer-motion";
 import { letterAnmi } from "../animations/animations";
 
 const sectionTitleVariants = {
-    animate: {
-        transition: {
-            delayChildren: 0.5,
-            staggerChildren: 0.03,
-        },
-    },
+   animate: {
+      transition: {
+         delayChildren: 0.5,
+         staggerChildren: 0.03,
+      },
+   },
 };
 
 function AnimatedSectionTitle(props: { Title: string }) {
-    return (
-        <motion.pre
-            variants={sectionTitleVariants}
-            initial="initial"
-            whileInView="animate"
-            viewport={{
-                once: true,
-            }}
-            className="text-light-1 font-bebas  text-[5.5rem] flex pl-10 overflow-y-hidden"
-        >
-            {props.Title.split("").map((letter, index) => (
-                <motion.span variants={letterAnmi} key={index}>
-                    {letter}
-                </motion.span>
-            ))}
-        </motion.pre>
-    );
+   return (
+      <motion.pre
+         variants={sectionTitleVariants}
+         initial="initial"
+         whileInView="animate"
+         viewport={{
+            once: true,
+         }}
+         className="text-light-1 font-bebas  text-[5.5rem] lg:text-[4.5rem] md:text-[2.5rem] flex pl-10 overflow-y-hidden"
+      >
+         {props.Title.split("").map((letter, index) => (
+            <motion.span variants={letterAnmi} key={index}>
+               {letter}
+            </motion.span>
+         ))}
+      </motion.pre>
+   );
 }
 
 export default AnimatedSectionTitle;
