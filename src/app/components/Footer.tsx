7517d4b8e04c0f7fbb19fc46e0c02a205c33230b
yasser110306 <yasserrabie110306@gmail.com>
2024-05-06 11:44:14 +0300

    make Footer component

---
+++ src/app/components/Footer.tsx
@@ -0,0 +1,14 @@
+function Footer() {
+   return (
+      <footer>
+         <div className="container bg-black">
+            <div className="flex justify-between items-center p-10">
+               <p className="text-white text-sm">© 2024 YasserRABIE</p>
+               <p className="text-white text-sm">Made with ❤️ by ME</p>
+            </div>
+         </div>
+      </footer>
+   );
+}
+
+export default Footer;
