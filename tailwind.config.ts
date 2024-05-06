faa1d8664e078568358e63178465bca32fb2922a
yasser110306 <yasserrabie110306@gmail.com>
2024-05-06 11:43:42 +0300

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,40 +1,40 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
-    content: [
-        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    ],
-    theme: {
-        extend: {
-            colors: {
-                "light-1": "#efefeb",
-                "light-2": "#DEDED7",
-                "dark-1": "#3A3733",
-                "dark-2": "#6E6860",
-                "dark-primary": "#202020",
-                "dark-secondry": "#020617",
-            },
-            fontFamily: {
-                madimi: ["Madimi One", "sans-serif"],
-                bebas: ["Bebas Neue", "sans-serif"],
-                montserrat: ["Montserrat", "sans-serif"],
-                japanese: ["Slackside One", "cursive"],
-                chinese: ["Zhi Mang Xing", "cursive"],
-                archivo: ["Archivo Black", "sans-serif"],
-                francios: ["Francois One", "sans-serif"],
-                avantn: ["avantn", "sans-serif"],
-            },
-            screens: {
-                "2xl": { max: "1400px" },
-                xl: { max: "1279px" },
-                lg: { max: "1023px" },
-                md: { max: "767px" },
-                sm: { max: "639px" },
-            },
-        },
-    },
-    plugins: [],
+   content: [
+      "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+      "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+   ],
+   theme: {
+      screens: {
+         "2xl": { max: "1400px" },
+         xl: { max: "1279px" },
+         lg: { max: "1023px" },
+         md: { max: "767px" },
+         sm: { max: "639px" },
+      },
+      extend: {
+         colors: {
+            "light-1": "#efefeb",
+            "light-2": "#DEDED7",
+            "dark-1": "#3A3733",
+            "dark-2": "#6E6860",
+            "dark-primary": "#202020",
+            "dark-secondry": "#020617",
+         },
+         fontFamily: {
+            madimi: ["Madimi One", "sans-serif"],
+            bebas: ["Bebas Neue", "sans-serif"],
+            montserrat: ["Montserrat", "sans-serif"],
+            japanese: ["Slackside One", "cursive"],
+            chinese: ["Zhi Mang Xing", "cursive"],
+            archivo: ["Archivo Black", "sans-serif"],
+            francios: ["Francois One", "sans-serif"],
+            avantn: ["avantn", "sans-serif"],
+         },
+      },
+   },
+   plugins: [],
 };
 export default config;
